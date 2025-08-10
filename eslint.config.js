70b938e74506b33fd7c9b9db9dd7c4b5ce4a9385
Eugene Leontev <potehin@gmail.com>
2025-08-10 17:36:48 +0400

    telegram sdk + fullscreen + linter update

---
+++ eslint.config.js
@@ -0,0 +1,69 @@
+// ESLint v9 flat config compatible with TypeScript 5.8
+import js from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import reactPlugin from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+import jsxA11y from 'eslint-plugin-jsx-a11y';
+import reactRefresh from 'eslint-plugin-react-refresh';
+
+export default [
+  {
+    ignores: [
+      'dist',
+      'build'
+    ],
+  },
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    files: ['**/*.{ts,tsx}'],
+    languageOptions: {
+      parser: tseslint.parser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        ecmaFeatures: { jsx: true },
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tseslint.plugin,
+      react: reactPlugin,
+      'react-hooks': reactHooks,
+      'jsx-a11y': jsxA11y,
+      'react-refresh': reactRefresh,
+    },
+    settings: {
+      react: { version: 'detect' },
+    },
+    rules: {
+      // React rules
+      'react-refresh/only-export-components': 'off',
+      'react/prop-types': 'off',
+      'react/react-in-jsx-scope': 'off',
+      'react/jsx-uses-react': 'off',
+
+      // TypeScript rules
+      '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_', varsIgnorePattern: '^_' }],
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-var-requires': 'error',
+
+      // General rules
+      'no-console': 'off',
+      'prefer-const': 'error',
+      'no-var': 'error',
+      'object-shorthand': 'off',
+      'prefer-template': 'off',
+
+      // Accessibility rules (relaxed for this project)
+      'jsx-a11y/alt-text': 'off',
+      'jsx-a11y/anchor-has-content': 'off',
+      'jsx-a11y/anchor-is-valid': 'off',
+      'jsx-a11y/click-events-have-key-events': 'off',
+      'jsx-a11y/no-static-element-interactions': 'off',
+    },
+  },
+];
+
+
--- imports/HayouBlock-6-101.tsx
+++ imports/HayouBlock-6-101.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+// React import removed (not needed with JSX transform)
 import svgPaths from "./svg-w3gxba1tai";
 import { useI18n } from "../src/hooks/useI18n";
 
